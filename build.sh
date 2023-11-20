8411c51111cd5777b2127d6aed4521d5cc6fc343
phi <ficarde@gmail.com>
2023-11-20 16:24:44 -0300

    loading screen for stage previews

---
+++ build.sh
@@ -0,0 +1,36 @@
+ext_exists() {
+  {
+    for x in *.$1; do
+      [ "${x%%\**}" = "$x" ] && return 0;
+    done
+  } 2> /dev/null
+  return 1
+}
+
+git_ext_exists() {
+  x=$(git status "*.$1")
+  case "$x" in
+    *"Untracked files"*) return 0; ;;
+    *) return 1; ;;
+  esac
+}
+
+ext_exists css && rm *.css
+git_ext_exists css && git rm -f '*.css'
+
+ext_exists js && rm *.js
+git_ext_exists js && git rm -f '*.js'
+
+ext_exists wasm && rm *.wasm
+git_ext_exists wasm && git rm -f '*.wasm'
+
+[ -d dist ] && git rm -rf dist
+[ -d dist ] && rm -r dist
+
+(
+  cd ../banana_pass
+  trunk build --release --public-url BananaPassword
+)
+
+cp ../banana_pass/dist/* .
+
